0902f3ef4fcc1cd0048de55413458447a132fa08
mouad mennioui <moad.menniui@gmail.com>
2024-05-16 20:06:54 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -52,3 +52,18 @@ def solve_nqueens(n):
     """
     board = [-1] * n
     solve_nqueens_util(board, 0, n)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solve_nqueens(N)
