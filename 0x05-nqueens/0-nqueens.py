33e15ba7ec7cd1fcfc9d4a89922a1661510a25a1
mouad mennioui <moad.menniui@gmail.com>
2024-05-16 19:50:41 +0100

    Create 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+
+
+def is_safe(board, row, col):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens_util(board, row, n):
+    if row == n:
+        print([[i, board[i]] for i in range(n)])
+        return
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens_util(board, row + 1, n)
+            board[row] = -1
+
+
+def solve_nqueens(n):
+    board = [-1] * n
+    solve_nqueens_util(board, 0, n)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solve_nqueens(N)
