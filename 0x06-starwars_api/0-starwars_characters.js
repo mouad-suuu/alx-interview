ccefb63dd2324244e78cbf3388b00803c6db596a
mouad mennioui <moad.menniui@gmail.com>
2024-05-23 23:50:48 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,25 +3,49 @@ const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      (url) =>
-        new Promise((resolve, reject) => {
-          request(url, (promiseErr, __, charactersReqBody) => {
-            if (promiseErr) {
-              reject(promiseErr);
-            }
-            resolve(JSON.parse(charactersReqBody).name);
-          });
-        })
-    );
+  const movieId = process.argv[2];
+  const filmUrl = `${API_URL}/films/${movieId}/`;
 
-    Promise.all(charactersName)
-      .then((names) => console.log(names.join('\n')))
-      .catch((allErr) => console.log(allErr));
+  // Request movie data
+  request(filmUrl, (err, res, body) => {
+    if (err) {
+      console.error('Error:', err);
+      return;
+    }
+    if (res.statusCode !== 200) {
+      console.error(`Failed to fetch data: ${res.statusCode}`);
+      return;
+    }
+
+    const filmData = JSON.parse(body);
+    const charactersURLs = filmData.characters;
+
+    // Fetch character names
+    const charactersPromises = charactersURLs.map((url) => {
+      return new Promise((resolve, reject) => {
+        request(url, (charErr, charRes, charBody) => {
+          if (charErr) {
+            reject(charErr);
+            return;
+          }
+          if (charRes.statusCode !== 200) {
+            reject(new Error(`Failed to fetch data: ${charRes.statusCode}`));
+            return;
+          }
+          resolve(JSON.parse(charBody).name);
+        });
+      });
+    });
+
+    // Print character names
+    Promise.all(charactersPromises)
+      .then((names) => {
+        console.log(names.join('\n'));
+      })
+      .catch((allErr) => {
+        console.error('Error fetching character names:', allErr);
+      });
   });
+} else {
+  console.error('Please provide a Movie ID as an argument.');
 }
